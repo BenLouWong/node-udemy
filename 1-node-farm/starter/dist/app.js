2d1f09b0314b402ab47ba035fd709cd0bac6a46c
BenLouWong <72722548+BenLouWong@users.noreply.github.com>
2022-12-31 11:32:11 +1100

    Populating the html template and building simple api

--- 1-node-farm/starter/dist/app.js
+++ 1-node-farm/starter/dist/app.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-// Synchronous code
+const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
+__dirname = path_1.default.resolve(path_1.default.dirname(''));
 // function synchronousCall(): void {
 //     const readFileText = fs.readFileSync('./txt/input.txt', 'utf-8');
 //     console.log(readFileText);
@@ -13,19 +15,54 @@ const fs_1 = __importDefault(require("fs"));
 //     console.log('File written');
 // }
 // synchronousCall();
-function asynchronousCall() {
-    fs_1.default.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-        fs_1.default.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-            console.log(data2);
-            fs_1.default.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-                console.log(data3);
-                fs_1.default.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', () => {
-                    console.log('File written');
+// function asynchronousCall(): void {
+//     fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//         fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//             console.log(data2);
+//             fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//                 console.log(data3);
+//                 fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', () => {
+//                     console.log('File written');
+//                 });
+//             });
+//         });
+//     });
+// }
+// asynchronousCall();
+// console.log('Will read file!');
+const fileData = fs_1.default.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = readApiResponse(fileData);
+function createBasicServer() {
+    const server = http_1.default.createServer((req, res) => {
+        const pathName = req.url;
+        switch (pathName) {
+            case '/':
+            case '/overview':
+                res.end('This is the OVERVIEW');
+                break;
+            case '/product':
+                res.end('This is the PRODUCT');
+                break;
+            case '/api':
+                res.writeHead(200, { 'Content-type': 'application/json' });
+                res.write(JSON.stringify(productData));
+                res.end();
+                break;
+            default:
+                res.writeHead(404, {
+                    'Content-type': 'text/html'
                 });
-            });
-        });
+                res.end('<h1>404 Page not found</h1>');
+                break;
+        }
+    });
+    server.listen(9000, '127.0.0.1', () => {
+        console.log('Listening to requests on port 9000');
     });
 }
-asynchronousCall();
-console.log('Will read file!');
+createBasicServer();
+function readApiResponse(data) {
+    const product = JSON.parse(data);
+    return product;
+}
 //# sourceMappingURL=app.js.map
--- 1-node-farm/starter/src/app.ts
+++ 1-node-farm/starter/src/app.ts
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import http from 'http';
+import path from 'path';
 
-// Synchronous code
+__dirname = path.resolve(path.dirname(''));
+
 // function synchronousCall(): void {
 //     const readFileText = fs.readFileSync('./txt/input.txt', 'utf-8');
 //     console.log(readFileText);
@@ -11,18 +14,71 @@ import fs from 'fs';
 // }
 // synchronousCall();
 
-function asynchronousCall(): void {
-    fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-        fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-            console.log(data2);
-            fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-                console.log(data3);
-                fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', () => {
-                    console.log('File written');
+// function asynchronousCall(): void {
+//     fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//         fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//             console.log(data2);
+//             fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//                 console.log(data3);
+//                 fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', () => {
+//                     console.log('File written');
+//                 });
+//             });
+//         });
+//     });
+// }
+// asynchronousCall();
+// console.log('Will read file!');
+
+const fileData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = readApiResponse(fileData);
+
+function createBasicServer(): void {
+    const server = http.createServer((req, res) => {
+        const pathName = req.url as PathName;
+        switch (pathName) {
+            case '/':
+            case '/overview':
+                res.end('This is the OVERVIEW');
+                break;
+            case '/product':
+                res.end('This is the PRODUCT');
+                break;
+            case '/api':
+                res.writeHead(200, { 'Content-type': 'application/json' });
+                res.write(JSON.stringify(productData));
+                res.end();
+                break;
+            default:
+                res.writeHead(404, {
+                    'Content-type': 'text/html'
                 });
-            });
-        });
+                res.end('<h1>404 Page not found</h1>');
+                break;
+        }
+    });
+
+    server.listen(9000, '127.0.0.1', () => {
+        console.log('Listening to requests on port 9000');
     });
 }
-asynchronousCall();
-console.log('Will read file!');
+createBasicServer();
+
+function readApiResponse(data: string): Product[] {
+    const product = JSON.parse(data) as Product[];
+    return product;
+}
+
+type PathName = '/overview' | '/product' | '/' | '/api';
+
+interface Product {
+    id: number;
+    productName: string;
+    image: string;
+    from: string;
+    nutrients: string;
+    quantity: string;
+    price: string;
+    organic: boolean;
+    description: string;
+}
